ea8e8c37a380999c5d618b9b0e877b6365c2b1ae
David Leiva <david.leiva04@gmail.com>
2019-05-09 21:27:40 -0600

    CORS solved

--- src/index.js
+++ src/index.js
@@ -17,6 +17,21 @@ app.use(conn(mysql, {
     database: 'pruebas'
 }, 'single'));
 
+// codigo para evitar CORS
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin,X-Requested-With,Content-Type,Accept,Authorization'
+    );
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, PUT, DELETE');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+
 app.use('/api/sf',require('./routes/routes'));   //explicar
 
 app.listen(app.get('port'),()=>{
