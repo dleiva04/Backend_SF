676152ed4fbcb3feed2a31409ecb21eee775c6f8
David Leiva <david.leiva04@gmail.com>
2019-05-18 01:12:10 -0600

    orden de las rutas, admin panel (registro)

---
+++ src/routes/controller.js
@@ -0,0 +1,101 @@
+const bcrypt = require('bcryptjs');
+module.exports = {
+     /*plantilla: async (req, res, ans) => {
+
+     }*/
+     iniciarSesionWeb: async (req, res, ans) => {
+          req.getConnection((err, conn) => {
+               conn.query(`call Login('${req.body.Email_User}')`, (err, user) => {
+                    if (err) {
+                         console.log(err);
+                    }
+                    console.log(user[0][0].Password_User);
+                    bcrypt.compare(req.body.Password_User, user[0][0].Password_User).then((response) => {
+                         if (response === true) {
+                              res.json(user[0][0]);
+                         } else {
+                              res.json({ 'mensaje': 'Usuario no registrado' });
+                         }
+                    });
+               });
+          });
+     },
+     registroWeb: async (req, res, ans) => {
+          bcrypt.genSalt(10, function (err, salt) {
+               bcrypt.hash(req.body.Password_User, salt, function (err, hash) {
+                    req.getConnection((err, conn) => {
+                         conn.query(`call Register('${req.body.Name_User}','${hash}','${req.body.Email_User}','${req.body.Phone_User}')`, (err, user) => {
+                              if (err) {
+                                   res.json(err);
+                              }
+                              res.json(user);
+                         });
+                    });
+               })
+          });
+     },
+     adminRegister: async (req, res, ans) => {
+          bcrypt.genSalt(10, function (err, salt) {
+               bcrypt.hash(req.body.Password_Admin, salt, function (err, hash) {
+                    req.getConnection((err, conn) => {
+                         conn.query(`call Register_Admin('${req.body.User_Admin}','${hash}','${req.body.Role_Admin}')`, (err, user) => {
+                              if (err) {
+                                   res.json({ 'error': 'Fallo en el registro' });
+                                   console.log(err);
+                              }
+                              res.json({ 'mensaje': 'Registrado con exito' });
+                         });
+                    });
+               })
+          });
+     },
+     adminLogin: async (req, res, ans) => {
+          req.getConnection((err, conn) => {
+               conn.query(`call AdminLogin('${req.body.User_Admin}')`, (err, user) => {
+                    if (err) {
+                         console.log(err);
+                    }
+                    bcrypt.compare(req.body.Password_Admin, user[0][0].Password_Admin).then((response) => {
+                         if (response === true) {
+                              res.json({ 'mensaje': 'Bienvenido' });
+                         } else {
+                              res.json({ 'error': 'Usuario y/o contraseña incorrecta' });
+                         }
+                    });
+               });
+          });
+     },
+     guardarCuponUnico: async (req, res, ans) => {
+          req.getConnection((err, conn) => {
+               conn.query(`call AdminLogin('${req.body.User_Admin}')`, (err, user) => {
+                    if (err) {
+                         console.log(err);
+                    }
+                    bcrypt.compare(req.body.Password_Admin, user[0][0].Password_Admin).then((response) => {
+                         if (response === true) {
+                              res.json({ 'mensaje': 'Bienvenido' });
+                         } else {
+                              res.json({ 'error': 'Usuario y/o contraseña incorrecta' });
+                         }
+                    });
+               });
+          });
+     },
+     guardarCuponTodos: async (req, res, ans) => {
+          req.getConnection((err, conn) => {
+               conn.query(`call AdminLogin('${req.body.User_Admin}')`, (err, user) => {
+                    if (err) {
+                         console.log(err);
+                    }
+                    bcrypt.compare(req.body.Password_Admin, user[0][0].Password_Admin).then((response) => {
+                         if (response === true) {
+                              res.json({ 'mensaje': 'Bienvenido' });
+                         } else {
+                              res.json({ 'error': 'Usuario y/o contraseña incorrecta' });
+                         }
+                    });
+               });
+          });
+     }
+
+};
